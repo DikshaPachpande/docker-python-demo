1b891ef60adafc4762d01ba7a20842a1fb8da8ea
DikshaPachpande <dikshapachpande@gmail.com>
2026-04-13 11:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello Diksha! Your Docker App is running 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+print(" hello welcome to docker !!!")
